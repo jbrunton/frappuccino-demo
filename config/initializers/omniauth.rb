5e6b288c62f9e61551e363b34cd5a28c5bc3ba76
jbrunton <john_brunton@hotmail.co.uk>
2012-09-07 21:28:18 +0100

    added in production auth keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '378336508905387', '640010b35299fa73c27f5a60b6253b9b'
+    if Rails.env == "production"
+        provider :facebook, '440801059299022', '6252372745f2d2e6d0c58b8ed9c1709f'
+    else
+        provider :facebook, '378336508905387', '640010b35299fa73c27f5a60b6253b9b'
+    end
 end
